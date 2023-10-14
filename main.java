248eb1e065ea50e9102158b98ff06db00eb6deb1
Parityfp <95124738+Parityfp@users.noreply.github.com>
2023-10-14 12:52:00 +0000

    base class

--- main.java
+++ main.java
@@ -1,19 +1,42 @@
 package Jproject; import java.util.*;import java.io.*;
 
 class Hotel{
-
+    protected String type;
+    protected String name;
 }
 
 class Room extends Hotel{
+    private double rate;
 
+    public Room(){
+
+    }
 }
 
 class Meal extends Hotel{
-    
+    private double rate;
+
+    public Meal(){
+        
+    }
+}
+
+class booking(
+    private int ID;
+    private String name;
+    private int nights;
+
+    private int type[];
+)
+
+class customers extends booking{
+
 }
 
 public class main{
     public static void main(String[] args){
-        System.out.println
+
+
+        
     }
 }
