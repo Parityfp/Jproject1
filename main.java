f3597c18856551c435a0804fd8dce2fe546e4f63
Parityfp <95124738+Parityfp@users.noreply.github.com>
2023-10-14 13:03:55 +0000

    some constructors

--- main.java
+++ main.java
@@ -3,12 +3,19 @@ package Jproject; import java.util.*;import java.io.*;
 class Hotel{
     protected String type;
     protected String name;
+
+    public Hotel(String type, String name){
+        this.type = type;
+        this.name = name;
+    }
 }
 
 class Room extends Hotel{
     private double rate;
 
-    public Room(){
+    public Room(String type, String name, double rate){
+        super(type, name);
+        this.rate = rate;
 
     }
 }
@@ -16,18 +23,21 @@ class Room extends Hotel{
 class Meal extends Hotel{
     private double rate;
 
-    public Meal(){
-        
+    public Meal(String type, String name, double rate){
+        super(type, name);
+        this.rate = rate;
+
     }
 }
 
-class booking(
+class booking{
     private int ID;
     private String name;
     private int nights;
 
     private int type[];
-)
+
+}
 
 class customers extends booking{
 
@@ -35,8 +45,10 @@ class customers extends booking{
 
 public class main{
     public static void main(String[] args){
+        ArrayList<Booking> AllBookings = new ArrayList<>();
+        ArrayList<Hotel> products = new ArrayList<>();
+        Scanner scanner;
 
 
-        
     }
 }
