eca9d3de854c3c91eaad8a7d5f801969bb0e1ba6
Parityfp <95124738+Parityfp@users.noreply.github.com>
2023-10-14 12:45:05 +0000

    dddd

--- main.java
+++ main.java
@@ -1,5 +1,17 @@
 package Jproject; import java.util.*;import java.io.*;
 
+class Hotel{
+
+}
+
+class Room extends Hotel{
+
+}
+
+class Meal extends Hotel{
+    
+}
+
 public class main{
     public static void main(String[] args){
         System.out.println
