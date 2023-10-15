a57625670fa56ec0bf217f1d101d645bcdf858d9
NineDERP <132637581+NineDERP@users.noreply.github.com>
2023-10-15 18:44:30 +0700

    Add files via upload

--- source.java
+++ source.java
@@ -1,6 +1,6 @@
 package Jproject1; import java.util.*;import java.io.*;
 
-class Hotel{
+ class Hotel {
     protected String type;
     protected String name;
 
@@ -8,28 +8,42 @@ class Hotel{
         this.type = type;
         this.name = name;
     }
-}
+   // public int compareTo(Hotel other){return 0;}
+    }
 
-class Room extends Hotel{
+class Room extends Hotel implements Comparable<Room> {
     private double rate;
-    private double rateVAT;
+    private double rateVAT, ttearning;
+    private int ttsales=0;
 
     public Room(String type, String name, double rate){
         super(type, name);
         this.rate = rate;
         rateVAT = (rate+(rate * 0.1))+((rate+(rate * 0.1))*0.07);
 
-    }       
+    }
     
+   @Override
+    public int compareTo(Room other) {
+     Room n = (Room) other;
+     if(this.ttsales<other.ttsales) return 1;
+     else if (this.ttsales>other.ttsales) return -1;
+     else return 0;
+    }
     public void printRoom() { System.out.printf("%-15s    rate = %,8.2f      rate++ = %,8.2f\n",name, rate, rateVAT);}
     //public String getRtype() {return type;}
     public String getRname () {return name;}
     public double getRate() {return rate;}
     public double getRateVAT() {return rateVAT;}
+    public void countsales(int i,int j) { ttsales= ttsales+(i*j); ttearning = ttsales*rateVAT ;}
+    public int getttsales() {return ttsales;}
+    public double getttearning() {return ttearning;}
+    
 }
 
 class Meal extends Hotel{
-    private double rate;
+    private double rate, ttearning;
+    private int ttsales=0;
 
     public Meal(String type, String name, double rate){
         super(type, name);
@@ -39,6 +53,9 @@ class Meal extends Hotel{
     public void printMeal() { System.out.printf("%-15s    rate = %,8.2f      rate++ = %,8.2f\n\n",name, rate, rate);}
     public String getname() {return name;}
     public double getRate() {return rate;}
+    public void countsales(int i,int j) { ttsales= ttsales+(i*j); ttearning = ttsales*rate ;}
+    public int getttsales() {return ttsales;}
+    public double getttearning() {return ttearning;}
 }
 
 class booking {
@@ -60,7 +77,7 @@ class booking {
     }
     
     public int getBID() {return ID;}
-    public String getBname() {return name;}
+    public String getBname() { return name;}
     public int getBnights() {return nights;}
     public int gettype(int[] type, int i) {return type[i];}
     public int[] getarraytype() {return type;}
@@ -77,6 +94,7 @@ class booking {
     }
     
     public int getcashback() {return cashback;}
+    public void setcashback(int i){cashback = i;}
     
     
     
@@ -92,6 +110,7 @@ public class source{
         H.openHotelLoop();
         H.openBookingLoop();
         H.Process();
+        H.summary();
     }
 }
 
@@ -211,8 +230,9 @@ class FileReader {
             {
             booking BO = new booking(ID, name, nights, t);
             B.add(BO);
-            C[j] = new customers(ID, name, nights, t , 0);
-            
+            for(int i=0 ; i<4;i++) H[i].countsales(nights, t[i]);
+            M.countsales(nights, t[4]);
+            //C[j] = new customers(ID, name, nights, t , 0);
             //customers CO = new customers(ID, name, nights, t, 0);
             //C.add(CO);
             
@@ -283,7 +303,9 @@ class FileReader {
         }
         
         public void Process(){
+            int j = 0;
         System.out.println("=== Booking Processing ===");
+        for(int i=0; i<100; i++) C[i] = new customers(i,"a",i,null,0);
         for (booking n: B) 
             
            {//print first line //need to redo formating 
@@ -291,20 +313,76 @@ class FileReader {
                , n.getBID(),n.getBname(),n.getBnights(),H[0].getRname(),n.gettype(n.getarraytype(),0)
                ,H[1].getRname(),n.gettype(n.getarraytype(),1),H[2].getRname(),n.gettype(n.getarraytype(), 2)
                ,H[3].getRname(),n.gettype(n.getarraytype(), 3),M.getname(),n.gettype(n.getarraytype(), 4));
-            
-            double ttroomprice = (((n.gettype(n.getarraytype(),0)*H[0].getRateVAT())+(n.gettype(n.getarraytype(),1)*H[1].getRateVAT())
-                    +(n.gettype(n.getarraytype(),2)*H[2].getRateVAT())+(n.gettype(n.getarraytype(),3)*H[3].getRateVAT()))* n.getBnights());
+                double ttroomprice = 0;
+            for (int i=0; i<4 ;i++){
+                ttroomprice = ttroomprice + (n.getBnights()*n.gettype(n.getarraytype(), i)*H[i].getRateVAT());
+               
+            }
             double ttmealprice = (n.gettype(n.getarraytype(),4)*M.getRate())*n.getBnights();
-            System.out.printf("Available cashback = %,5d >> total room price++ = %,10.2f   With service charge and VAT \n", C[0].getcashback(), ttroomprice );
-            System.out.printf(">> total meal price = %,10.2f \n",ttmealprice);
-            System.out.printf(">> total bill = %,10.2f redeem = %,5d\n",ttroomprice+ttmealprice,C[0].getcashback());
-            System.out.printf(">> Final bill = %,10.2f cashback for next booking = %,5d\n\n", ttroomprice+ttmealprice-C[0].getcashback(), (int)(ttroomprice*0.05));
+            
+            int cashback = (int)(ttroomprice*0.05);
+            
+            
+            for(int i =0; i<100; i++){
+            if(n.getBname().compareTo(C[i].getBname())==0) {
+              
+                System.out.printf("Available cashback = %,5d >> total room price++ = %,10.2f   With service charge and VAT \n", C[i].getcashback(), ttroomprice );
+                if(C[i].getcashback()>((ttroomprice+ttmealprice)/2)) {
+                    cashback= cashback + C[i].getcashback()-((int)(ttroomprice+ttmealprice)/2);
+                    C[i].setcashback((int)(ttroomprice+ttmealprice)/2);
+                }
+                System.out.printf(">> total meal price = %,10.2f \n",ttmealprice);
+                System.out.printf(">> total bill = %,10.2f redeem = %,5d\n",ttroomprice+ttmealprice,C[i].getcashback());
+                System.out.printf(">> Final bill = %,10.2f cashback for next booking = %,5d\n\n", ttroomprice+ttmealprice-C[i].getcashback(), cashback );
+                C[i].setcashback(cashback);
+                
+              
+                break ;
+            }
+            //else if (Integer.parseInt(C[i].getBname().trim())==i) break; 
+            else if(C[i].getBname().compareTo("a")==0)
+            {
+                System.out.printf("Available cashback = %,5d >> total room price++ = %,10.2f   With service charge and VAT \n", 0, ttroomprice );
+                System.out.printf(">> total meal price = %,10.2f \n",ttmealprice);
+                System.out.printf(">> total bill = %,10.2f redeem = %,5d\n",ttroomprice+ttmealprice,0);
+                System.out.printf(">> Final bill = %,10.2f cashback for next booking = %,5d\n\n", ttroomprice+ttmealprice, cashback);
+                C[j] = new customers(n.getBID(), n.getBname(), n.getBnights(), n.getarraytype() , cashback); j++;    
+                break;}
+       
+             }
+           
+            
+           
             
             
             
             
            }
+        //for (int i = 0; i<20; i++)
+            //System.out.println(C[i].getBname());
+      
+        
+        
         }
+        
+       public void summary(){
+           
+           System.out.println("=== Room Summary ===");
+            Arrays.sort(H);
+           for(int i=0 ; i<4;i++)
+       {
+            System.out.printf("%-20s total sales = %,6d rooms %,15.2f Baht\n", H[i].getRname(),H[i].getttsales(),H[i].getttearning());
+       }
+           System.out.println("");
+           System.out.println("=== Meal Summary ===");
+           System.out.printf("%-20s total sales = %,6d heads %,15.2f Baht\n", M.getname(),M.getttsales(),M.getttearning());
+            
+       }
+       
+       public void cashback(booking n){
+       
+       
+       }
     }
 
 
